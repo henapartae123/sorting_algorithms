989f767a0523817874d11e12f43a5378bb34a236
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 18:56:30 +0300

    sort

--- sort.h
+++ sort.h
@@ -28,4 +28,5 @@ void selection_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void shell_sort(int *array, size_t size);
 #endif

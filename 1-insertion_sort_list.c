69dee7c43586325968f7093d55802c05335e1930
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 18:48:34 +0300

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,61 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a linked list
+ * len_list - returns the length of a linked list
+ * @h: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *h)
+{
+    int len = 0;
+
+    while (h)
+    {
+        len++;
+        h = h->next;
+    }
+    return (len);
+}
+
+/**
+ * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * @list: double pointer to the list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node, *tmp, *next, *tmp_next, *tmp_prev;
-    int i = 0;
+    listint_t *curr = NULL, *one = NULL;
+    listint_t *two = NULL, *three = NULL, *four = NULL;
 
-    if (list == NULL || (*list)->next == NULL)
+    if (!list || !(*list) || len_list(*list) < 2)
         return;
-    node = *list;
-    node = node->next;
-    while (node != NULL)
-    {
-        tmp = node;
-        next = node->next;
-        tmp_next = tmp->next;
-        tmp_prev = tmp->prev;
 
-        while (tmp->prev != NULL && tmp_prev->n > tmp->n)
+    curr = *list;
+
+    while (curr)
+    {
+        if (curr->prev && curr->n < curr->prev->n)
         {
-            if (tmp_next != NULL)
-                tmp_next->prev = tmp_prev;
-            tmp_prev->next = tmp_next;
-            if (tmp_prev->prev == NULL)
-            {
-                tmp_prev->prev = tmp;
-                *list = tmp;
-                tmp->prev = NULL;
-            }
+            one = curr->prev->prev;
+            two = curr->prev;
+            three = curr;
+            four = curr->next;
+
+            two->next = four;
+            if (four)
+                four->prev = two;
+            three->next = two;
+            three->prev = one;
+            if (one)
+                one->next = three;
             else
-            {
-                tmp->prev = tmp_prev->prev;
-                tmp_prev->prev->next = tmp;
-                tmp_prev->prev = tmp;
-            }
-            tmp->next = tmp_prev;
-            tmp_prev = tmp->prev;
-            tmp_next = tmp->next;
+                *list = three;
+            two->prev = three;
+            curr = *list;
             print_list(*list);
+            continue;
         }
-        node = next;
-        i++;
+        else
+            curr = curr->next;
     }
 }

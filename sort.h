dff921cc3b701e2dec4d50bc8e2405084f842aea
Henok Shewakena <henyvibe@gmail.com>
2022-12-15 19:08:26 +0300

    sort

--- sort.h
+++ sort.h
@@ -32,4 +32,8 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 
+void merge_sort(int *array, size_t size);
+void merge(int *array, int low, int middle, int high, int *temp);
+void merge_sorty(int *array, int low, int high, int *temp);
+
 #endif
